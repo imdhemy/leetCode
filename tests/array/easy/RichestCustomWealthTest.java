53375c310dfc6c2c64d785386481350a61bc050a
imdhemy <imdhemy@gmail.com>
2020-12-20 20:42:56 +0200

    Update RichestCustomWealthTest.java

--- tests/array/easy/RichestCustomWealthTest.java
+++ tests/array/easy/RichestCustomWealthTest.java
@@ -7,9 +7,9 @@ class RichestCustomWealthTest {
 
   @Test
   void maximumWealth() {
-    RichestCustomWealth $obj = new RichestCustomWealth();
-    Assertions.assertEquals(6, $obj.maximumWealth(new int[][]{{1, 2, 3}, {3, 2, 1}}));
-    Assertions.assertEquals(10, $obj.maximumWealth(new int[][]{{1, 5}, {7, 3}, {3, 5}}));
-    Assertions.assertEquals(17, $obj.maximumWealth(new int[][]{{2, 8, 7}, {7, 1, 3}, {1, 9, 5}}));
+    RichestCustomWealth obj = new RichestCustomWealth();
+    Assertions.assertEquals(6, obj.maximumWealth(new int[][]{{1, 2, 3}, {3, 2, 1}}));
+    Assertions.assertEquals(10, obj.maximumWealth(new int[][]{{1, 5}, {7, 3}, {3, 5}}));
+    Assertions.assertEquals(17, obj.maximumWealth(new int[][]{{2, 8, 7}, {7, 1, 3}, {1, 9, 5}}));
   }
 }
