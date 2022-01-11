1248abff1bcc267ee893339a7b54a7a29111f874
Mohamad Eldhemy <imdhemy@gmail.com>
2022-01-11 17:32:49 +0300

    solve fizz buzz problem

--- src/easy/FizzBuzz.java
+++ src/easy/FizzBuzz.java
@@ -1,4 +1,31 @@
 package easy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FizzBuzz {
+
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
+    public static final String FIZZBUZZ = "FizzBuzz";
+
+    public static final int NUMBER_FIZZ = 3;
+    public static final int NUMBER_BUZZ = 5;
+    public static final int NUMBER_FIZ_BUZZ = 15;
+
+    public static List<String> solution(int n) {
+        List<String> result = new ArrayList<>();
+
+        if (n < 1) return result;
+
+        for (int i = 1; i <= n; i++) {
+
+            if (i % NUMBER_FIZ_BUZZ == 0) result.add(FIZZBUZZ);
+            else if (i % NUMBER_FIZZ == 0) result.add(FIZZ);
+            else if (i % NUMBER_BUZZ == 0) result.add(BUZZ);
+            else result.add(String.valueOf(i));
+        }
+
+        return result;
+    }
 }
--- test/easy/FizzBuzzTest.java
+++ test/easy/FizzBuzzTest.java
@@ -1,12 +1,37 @@
 package easy;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.List;
 
 class FizzBuzzTest {
+
     @Test
-    void true_is_true() {
-        assertTrue(true);
+    void it_returns_list_containing_1_if_input_is_1() {
+        Assertions.assertEquals(List.of("1"), FizzBuzz.solution(1));
+    }
+
+    @Test
+    void it_returns_list_of_1_2_if_input_is_2() {
+        Assertions.assertEquals(List.of("1", "2"), FizzBuzz.solution(2));
+    }
+
+    @Test
+    void it_replaces_3_with_fizz() {
+        Assertions.assertEquals(List.of("1", "2", FizzBuzz.FIZZ), FizzBuzz.solution(3));
+    }
+
+    @Test
+    void it_replaces_5_with_buzz() {
+        Assertions.assertEquals(List.of("1", "2", FizzBuzz.FIZZ, "4", FizzBuzz.BUZZ), FizzBuzz.solution(5));
+    }
+
+    @Test
+    void it_replaces_3_and_5_divisible_by_FizzBuzz() {
+        Assertions.assertEquals(
+                List.of("1", "2", FizzBuzz.FIZZ, "4", FizzBuzz.BUZZ, FizzBuzz.FIZZ, "7", "8", FizzBuzz.FIZZ, FizzBuzz.BUZZ, "11", FizzBuzz.FIZZ, "13", "14", FizzBuzz.FIZZBUZZ),
+                FizzBuzz.solution(FizzBuzz.NUMBER_FIZ_BUZZ)
+        );
     }
 }
