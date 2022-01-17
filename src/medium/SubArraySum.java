0bf1b9f10922c518ebdef80efd03ef63ac201ea7
Mohamad Eldhemy <imdhemy@gmail.com>
2022-01-17 16:47:11 +0300

    Update SubArraySum.java

--- src/medium/SubArraySum.java
+++ src/medium/SubArraySum.java
@@ -1,18 +1,21 @@
 package medium;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SubArraySum {
     public static int solution(int[] nums, int k) {
         int count = 0;
+        int sum = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            int sum = 0;
-            for (int j = i; j < nums.length; j++) {
-                if (i == j) sum = nums[i];
-                else sum += nums[j];
-                if (sum == k) {
-                    count++;
-                }
-            }
+        Map<Integer, Integer> cumulativeSumMap = new HashMap<>();
+        cumulativeSumMap.put(0, 1);
+
+        for (int num : nums) {
+            sum += num;
+            int target = sum - k;
+            count += cumulativeSumMap.getOrDefault(target, 0);
+            cumulativeSumMap.put(sum, 1 + cumulativeSumMap.getOrDefault(sum, 0));
         }
 
         return count;
