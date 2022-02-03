728070a18031ebb687071e486715ad9a8f9b488b
Mohamad Eldhemy <imdhemy@gmail.com>
2022-02-03 16:54:40 +0300

    populate nums array on constructor

--- src/easy/NumArray.java
+++ src/easy/NumArray.java
@@ -5,14 +5,17 @@ public class NumArray {
     private final int[] nums;
 
     public NumArray(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
+
         this.nums = nums;
     }
 
     public int sumRange(int left, int right) {
-        int sum = nums[left];
-        for (int i = left + 1; i <= right; i++) {
-            sum += nums[i];
-        }
-        return sum;
+        if (left == 0)
+            return nums[right];
+
+        return nums[right] - nums[left - 1];
     }
 }
