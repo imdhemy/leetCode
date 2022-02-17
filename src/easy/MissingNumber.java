758a06bdcc2c96a7d95e1e04c9d61c7f9f2a4e53
Mohamad Eldhemy <imdhemy@gmail.com>
2022-02-17 21:20:41 +0300

    Update MissingNumber.java

--- src/easy/MissingNumber.java
+++ src/easy/MissingNumber.java
@@ -3,6 +3,7 @@ package easy;
 public class MissingNumber {
     public static int solution(int[] nums) {
         int xor = 0, i;
+
         for (i = 0; i < nums.length; i++) {
             xor = xor ^ i ^ nums[i];
         }
