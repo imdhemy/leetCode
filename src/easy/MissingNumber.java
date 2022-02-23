0ac067b12149deb675a36e0fb8a4641cfc376a41
Mohamad Eldhemy <imdhemy@gmail.com>
2022-02-23 16:10:10 +0300

    avoid integer overload

--- src/easy/MissingNumber.java
+++ src/easy/MissingNumber.java
@@ -4,10 +4,9 @@ public class MissingNumber {
     public static int solution(int[] nums) {
         double seqSum = ((double) (nums.length + 1) / 2) * nums.length;
 
-        int numSum = 0;
         for (int num : nums)
-            numSum += num;
+            seqSum -= num;
 
-        return (int) (seqSum - numSum);
+        return (int) seqSum;
     }
 }
