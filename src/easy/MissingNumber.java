5ae567aed35ab3e6b4b10c515ec0a9a4af1e71b1
Mohamad Eldhemy <imdhemy@gmail.com>
2022-02-24 22:52:10 +0300

    code refactoring

--- src/easy/MissingNumber.java
+++ src/easy/MissingNumber.java
@@ -2,13 +2,12 @@ package easy;
 
 public class MissingNumber {
     public static int solution(int[] nums) {
-        int a = nums.length, b = 0;
+        int missingNumber = nums.length;
 
         for (int i = 0; i < nums.length; i++) {
-            a ^= i;
-            b ^= nums[i];
+            missingNumber ^= i ^ nums[i];
         }
 
-        return a ^ b;
+        return missingNumber;
     }
 }
