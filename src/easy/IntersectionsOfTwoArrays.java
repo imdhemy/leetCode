347b2a134241c059f995e6c3efc3743f3be8602c
Mohamad Eldhemy <imdhemy@gmail.com>
2022-03-26 21:03:10 +0300

    use Hash Set to find the intersection

--- src/easy/IntersectionsOfTwoArrays.java
+++ src/easy/IntersectionsOfTwoArrays.java
@@ -5,18 +5,17 @@ import java.util.Set;
 
 public class IntersectionsOfTwoArrays {
     public int[] intersection(int[] nums1, int[] nums2) {
-        Set<Integer> set = new HashSet<>();
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> intersection = new HashSet<>();
 
-        for (int j : nums1) {
-            for (int k : nums2) {
-                if (j == k)
-                    set.add(j);
-            }
+        for (int num : nums1) set1.add(num);
+        for (int num : nums2) {
+            if (set1.contains(num)) intersection.add(num);
         }
 
-        int[] result = new int[set.size()];
+        int[] result = new int[intersection.size()];
         int i = 0;
-        for (Integer num : set)
+        for (int num : intersection)
             result[i++] = num;
 
         return result;
