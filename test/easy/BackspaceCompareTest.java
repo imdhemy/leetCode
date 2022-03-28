4c11ff22c9ca9c45ff95a3f2592b5910894ae83b
Mohamad Eldhemy <imdhemy@gmail.com>
2022-03-28 15:49:07 +0300

    Update BackspaceCompareTest.java

--- test/easy/BackspaceCompareTest.java
+++ test/easy/BackspaceCompareTest.java
@@ -8,9 +8,9 @@ class BackspaceCompareTest {
     @Test
     void backspaceCompare() {
         BackspaceCompare obj = new BackspaceCompare();
-//        Assertions.assertTrue(obj.backspaceCompare("ab#c", "ad#c"));
-//        Assertions.assertTrue(obj.backspaceCompare("ab##", "c#d#"));
-//        Assertions.assertFalse(obj.backspaceCompare("a#c", "b"));
+        Assertions.assertTrue(obj.backspaceCompare("ab#c", "ad#c"));
+        Assertions.assertTrue(obj.backspaceCompare("ab##", "c#d#"));
+        Assertions.assertFalse(obj.backspaceCompare("a#c", "b"));
         Assertions.assertTrue(obj.backspaceCompare("y#fo##f", "y#f#o##f"));
     }
 }
