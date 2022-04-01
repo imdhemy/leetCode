19fe738f2262f91fdca1bbfac69fcb52ec8b102f
Mohamad Eldhemy <imdhemy@gmail.com>
2022-04-01 16:39:20 +0300

    code refactoring

--- src/easy/LicenseKeyFormatter.java
+++ src/easy/LicenseKeyFormatter.java
@@ -1,25 +1,21 @@
 package easy;
 
-import java.util.Locale;
-
 public class LicenseKeyFormatter {
 
     public String licenseKeyFormatting(String s, int k) {
         StringBuilder sb = new StringBuilder();
-        char[] chars = s.toUpperCase(Locale.ROOT).toCharArray();
-        int dashes = 0;
 
-        for (int i = chars.length - 1; i >= 0; i--) {
-            char c = chars[i];
-            if (c != '-') {
-                if (sb.length() != 0 && (sb.length() - dashes) % k == 0) {
-                    sb.insert(0, "-");
-                    dashes++;
-                }
-                sb.insert(0, c);
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '-')
+                continue;
+
+            if ((sb.length()) % (k + 1) == k) {
+                sb.append("-");
             }
+
+            sb.append(Character.toUpperCase(s.charAt(i)));
         }
 
-        return sb.toString();
+        return sb.reverse().toString();
     }
 }
