ea45e0cc675f58f021206fdacd8a1970d6438405
Mohamad Eldhemy <imdhemy@gmail.com>
2022-04-16 16:57:46 +0300

    Update CheckPermutation.java

--- src/ctci/arraysandstrings/CheckPermutation.java
+++ src/ctci/arraysandstrings/CheckPermutation.java
@@ -1,5 +1,7 @@
 package ctci.arraysandstrings;
 
+import java.util.Arrays;
+
 public class CheckPermutation {
     public static boolean check(String a, String b) {
         if (a == null || b == null) return true;
@@ -21,17 +23,28 @@ public class CheckPermutation {
     }
 
     public static boolean checkInclusion(String s1, String s2) {
-        if (s1 == null || s2 == null) return false;
         if (s1.length() > s2.length()) return false;
 
-        int i = 0;
-        while (i < s2.length()) {
-            if (i + s1.length() > s2.length()) return false;
-            String substr = s2.substring(i, i + s1.length());
-            if (isPermutation(s1, substr)) return true;
-            i++;
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            arr1[s1.charAt(i) - 'a']++;
+            arr2[s2.charAt(i) - 'a']++;
         }
 
+        if (Arrays.equals(arr1, arr2)) return true;
+
+        int front = 0;
+        int back = s1.length();
+        while (back < s2.length()) {
+            arr2[s2.charAt(front) - 'a']--;
+            arr2[s2.charAt(back) - 'a']++;
+
+            if (Arrays.equals(arr1, arr2)) return true;
+            front++;
+            back++;
+        }
         return false;
     }
 
