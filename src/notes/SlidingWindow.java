aa0dc2362a8e21afbb577ee5b24f031c8f922c7f
Mohamad Eldhemy <imdhemy@gmail.com>
2022-04-17 11:20:07 +0300

    add sliding window max sum solution

--- src/notes/SlidingWindow.java
+++ src/notes/SlidingWindow.java
@@ -2,6 +2,8 @@ package notes;
 
 public class SlidingWindow {
     public int bruteForceMaxSum(int[] nums, int k) {
+        if (nums.length < k) return -1;
+
         int result = Integer.MIN_VALUE;
 
         for (int i = 0; i < nums.length - k + 1; i++)
@@ -17,5 +19,22 @@ public class SlidingWindow {
             sum += arr[start++];
 
         return sum;
+    }
+
+    public int slidingWindowMaxSum(int[] nums, int k) {
+        if (nums.length < k) return -1;
+
+        int windowSum = 0;
+        for (int i = 0; i < k; i++)
+            windowSum += nums[i];
+
+        int result = windowSum;
+
+        for (int i = k; i < nums.length; i++) {
+            windowSum = windowSum - nums[i - k] + nums[i];
+            result = Math.max(result, windowSum);
+        }
+
+        return result;
     }
 }
