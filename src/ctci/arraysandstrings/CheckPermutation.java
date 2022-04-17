c281dbbd9efb108d04cd9367544516582c306994
Mohamad Eldhemy <imdhemy@gmail.com>
2022-04-17 12:32:30 +0300

    add sliding window solution for check inclusion problem

--- src/ctci/arraysandstrings/CheckPermutation.java
+++ src/ctci/arraysandstrings/CheckPermutation.java
@@ -23,32 +23,33 @@ public class CheckPermutation {
     }
 
     public static boolean checkInclusion(String s1, String s2) {
+        if (s1 == null || s2 == null) return false;
         if (s1.length() > s2.length()) return false;
 
-        int[] arr1 = new int[26];
-        int[] arr2 = new int[26];
+        // Calculate freq of s1
+        int[] s1Freq = new int[26];
+        for (int i = 0; i < s1.length(); i++)
+            s1Freq[s1.charAt(i) - 'a']++;
 
-        for (int i = 0; i < s1.length(); i++) {
-            arr1[s1.charAt(i) - 'a']++;
-            arr2[s2.charAt(i) - 'a']++;
+        // Sliding window
+        int[] s2Freq = new int[26];
+        int start = 0, end = 0;
+
+        // Calculate freq of first window and check inclusion
+        while (end < s1.length()) {
+            s2Freq[s2.charAt(end) - 'a']++;
+            end++;
+        }
+        if (Arrays.equals(s1Freq, s2Freq)) return true;
+
+        // Sliding window
+        while (end < s2.length()) {
+            s2Freq[s2.charAt(start++) - 'a']--;
+            s2Freq[s2.charAt(end++) - 'a']++;
+
+            if (Arrays.equals(s1Freq, s2Freq)) return true;
         }
 
-        if (Arrays.equals(arr1, arr2)) return true;
-
-        int front = 0;
-        int back = s1.length();
-        while (back < s2.length()) {
-            arr2[s2.charAt(front) - 'a']--;
-            arr2[s2.charAt(back) - 'a']++;
-
-            if (Arrays.equals(arr1, arr2)) return true;
-            front++;
-            back++;
-        }
         return false;
-    }
-
-    private static boolean isPermutation(String a, String b) {
-        return check(a, b);
     }
 }
