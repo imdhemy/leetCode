13035a76b6872d907dc3daef8de2dda27d6bce34
Mohamad Eldhemy <imdhemy@gmail.com>
2022-05-04 10:11:15 +0300

    Update StringCompression.java

--- src/ctci/arraysandstrings/StringCompression.java
+++ src/ctci/arraysandstrings/StringCompression.java
@@ -3,20 +3,16 @@ package ctci.arraysandstrings;
 public class StringCompression {
     public String compress(String str) {
         StringBuilder sb = new StringBuilder();
-        sb.append(str.charAt(0));
         int freq = 1;
 
         for (int i = 1; i < str.length(); i++) {
-            if (str.charAt(i) == str.charAt(i - 1)) {
-                freq++;
-            } else {
-                sb.append(freq);
-                sb.append(str.charAt(i));
-                freq = 1;
+            freq++;
+
+            if (i + 1 >= str.length() || str.charAt(i) != str.charAt(i + 1)) {
+                sb.append(str.charAt(i)).append(freq);
+                freq = 0;
             }
         }
-
-        sb.append(freq);
 
         return sb.length() >= str.length() ? str : sb.toString();
     }
