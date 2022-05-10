80f50e51141b05174694f8a45774c35adf02adad
Mohamad Eldhemy <imdhemy@gmail.com>
2022-05-10 10:02:50 +0300

    Update ZeroMatrix.java

--- src/ctci/arraysandstrings/ZeroMatrix.java
+++ src/ctci/arraysandstrings/ZeroMatrix.java
@@ -4,21 +4,51 @@ import java.util.Arrays;
 
 public class ZeroMatrix {
     public void setZeros(int[][] matrix) {
-        boolean[] rows = new boolean[matrix.length];
-        boolean[] cols = new boolean[matrix[0].length];
+        boolean rowHasZero = false;
+        boolean colHasZero = false;
 
-        for (int i = 0; i < matrix.length; i++)
-            for (int j = 0; j < matrix[i].length; j++)
+        // check first row for zeros
+        for (int i = 0; i < matrix[0].length; i++)
+            if (matrix[0][i] == 0) {
+                rowHasZero = true;
+                break;
+            }
+
+        // check first col for zeros
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][0] == 0) {
+                colHasZero = true;
+                break;
+            }
+        }
+
+        // check the rest of the array for zeros
+        for (int i = 1; i < matrix.length; i++)
+            for (int j = 1; j < matrix[0].length; j++) {
                 if (matrix[i][j] == 0) {
-                    rows[i] = true;
-                    cols[j] = true;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
+            }
 
-        for (int i = 0; i < rows.length; i++)
-            if (rows[i]) nullifyRow(matrix, i);
 
-        for (int i = 0; i < cols.length; i++)
-            if (cols[i]) nullifyCol(matrix, i);
+        // nullify based on the values in the first column
+        for (int i = 1; i < matrix.length; i++)
+            if (matrix[i][0] == 0)
+                nullifyRow(matrix, i);
+
+        // nullify based on the values in the first row
+        for (int i = 1; i < matrix[0].length; i++)
+            if (matrix[0][i] == 0)
+                nullifyCol(matrix, i);
+
+        // nullify first row
+        if (rowHasZero)
+            nullifyRow(matrix, 0);
+
+        // nullify first col
+        if (colHasZero)
+            nullifyCol(matrix, 0);
     }
 
     private void nullifyCol(int[][] matrix, int col) {
