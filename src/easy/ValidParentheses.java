d389f7053ff271998f5a41665eff07a5eca4547f
Mohamad Eldhemy <imdhemy@gmail.com>
2022-06-17 12:45:30 +0300

    Update ValidParentheses.java

--- src/easy/ValidParentheses.java
+++ src/easy/ValidParentheses.java
@@ -4,31 +4,30 @@ import java.util.Stack;
 
 public class ValidParentheses {
     public boolean isValid(String s) {
-        if (null == s) return false;
-        if (s.length() == 0) return true;
-
         Stack<Character> stack = new Stack<>();
 
-        for (char c : s.toCharArray()) {
-            if (isOpening(c))
-                stack.push(c);
-            else if (stack.isEmpty()) {
-                return false;
-            } else if (c == '}') {
-                if (stack.pop() != '{')
-                    return false;
-            } else if (c == ']') {
-                if (stack.pop() != '[')
-                    return false;
-            } else {
-                if (stack.pop() != '(')
-                    return false;
+        for (char b : s.toCharArray()) {
+            if (isOpening(b)) {
+                stack.push(b);
+                continue;
             }
+
+            if (stack.isEmpty()) return false;
+
+            if (!isMatching(stack.pop(), b)) return false;
         }
+
         return stack.isEmpty();
     }
 
-    private boolean isOpening(char c) {
-        return c == '{' || c == '[' || c == '(';
+    private boolean isOpening(char b) {
+        return b == '(' || b == '[' || b == '{';
+    }
+
+    private boolean isMatching(char o, char c) {
+        return
+                o == '(' && c == ')' ||
+                        o == '[' && c == ']' ||
+                        o == '{' && c == '}';
     }
 }
