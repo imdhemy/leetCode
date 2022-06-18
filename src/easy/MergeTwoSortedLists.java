c0fbbbce3ec70cf1d52d71de5fac4406c62914da
Mohamad Eldhemy <imdhemy@gmail.com>
2022-06-18 13:50:56 +0300

    Update MergeTwoSortedLists.java

--- src/easy/MergeTwoSortedLists.java
+++ src/easy/MergeTwoSortedLists.java
@@ -2,28 +2,29 @@ package easy;
 
 public class MergeTwoSortedLists {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode head = null;
-        ListNode tail = null;
-        ListNode candidate;
+        ListNode head = null, tail = null, node;
 
         while (list1 != null || list2 != null) {
-            if (list2 == null || (list1 != null && list1.val < list2.val)) {
-                candidate = list1;
+            if (shouldTakeFirst(list1, list2)) {
+                node = list1;
                 list1 = list1.next;
             } else {
-                candidate = list2;
+                node = list2;
                 list2 = list2.next;
             }
 
-            if (head == null) {
-                head = candidate;
-            } else {
-                tail.next = candidate;
-            }
+            if (head == null) head = node;
+            else tail.next = node;
 
-            tail = candidate;
+            tail = node;
         }
 
         return head;
+    }
+
+    private boolean shouldTakeFirst(ListNode l1, ListNode l2) {
+        if (l2 == null) return true;
+        if (l1 == null) return false;
+        return l1.val < l2.val;
     }
 }
